39aae43560ac563222b15b7b6e44cc7bca04dbe4
ab.reza <a.re.morteza@gmail.com>
2019-05-11 11:42:27 +0430

    add error-codes

--- django_example/bibot_app/views.py
+++ django_example/bibot_app/views.py
@@ -7,13 +7,17 @@ def check_bibot_response(request):
     if request.POST.get('bibot-response') is not None:
         if request.POST.get('bibot-response') != '':
             r = requests.post('https://api.bibot.ir/api1/siteverify/', data={
-                'secretkey': 'your_secret_key',
+                'secret': 'your_site_key',
                 'response': request.POST['bibot-response']
             })
             print(r.json())
             if r.json()['success']:
                 messages.success(request, 'فرایند تایید هویت شما با موفقیت انجام شد!')
                 return True
+            elif r.json()['error-codes']:
+                for error_code in r.json()['error-codes']:
+                    messages.error(request, error_code)
+                return False
             else:
                 messages.error(request, 'بی‌بات به درستی حل نشده است!')
                 return False
