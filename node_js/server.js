faffe3e974b5eac60e3e94dba7dc13aa7e02dfcf
ab.reza <a.re.morteza@gmail.com>
2019-05-12 21:57:12 +0430

    update bibot messages

--- node_js/server.js
+++ node_js/server.js
@@ -3,19 +3,17 @@ const bodyParser = require('body-parser');
 const https = require("https");
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 const app = express();
-//app.use('/assets', express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
-
 
 app.get('/', function (req, res, next) {
     res.render("example.ejs");
 });
 
-app.post('/', urlencodedParser, function (req, res, next) {
-    let secret_key = "your secret key!";
+
+function request_to_bibot_server(secret, bibot_response, callback) {
     let data = JSON.stringify({
-        response: req.body["bibot-response"],
-        secretkey: secret_key
+        response: bibot_response,
+        secret: secret
     });
 
     const options = {
@@ -28,41 +26,72 @@ app.post('/', urlencodedParser, function (req, res, next) {
         }
     };
 
-    let body = "";
     const request = https.request(options, (response) => {
+        let body = "";
         console.log(`statusCode: ${response.statusCode}`);
         response.on('data', (d) => {
             body += d;
-            //process.stdout.write(d)
-
         });
 
         response.on('end', () => {
             body = JSON.parse(body);
-            if (body.success) {
-                res.render('example.ejs', {
-                    message: 'Your contact request have submitted successfully.',
-                    message_tag: "success"
-                });
-            } else {
-                res.render('example.ejs', {
-                    message: 'Robot verification failed, please try again.',
-                    message_tag: "error"
-                });
-            }
-
-
+            callback(body);
         });
 
     });
     request.on('error', (error) => {
         console.error(error)
     });
-
     request.write(data);
     request.end()
+}
 
 
+app.post('/', urlencodedParser, function (req, res, next) {
+    let secret = 'your_site_key';
+    if (req.body['bibot-response'] !== undefined) {
+        if (req.body['bibot-response'] !== '') {
+            request_to_bibot_server(secret, req.body['bibot-response'], function (bibot_server_response) {
+                let messages = [];
+                console.log(bibot_server_response);
+                if (bibot_server_response['success']) {
+                    messages.push({
+                        'text': 'فرایند تایید هویت شما با موفقیت انجام شد!',
+                        'tag': 'success'
+                    });
+                } else if (bibot_server_response['error-codes']) {
+                    for (let error_code in bibot_server_response['error-codes']) {
+                        messages.push({
+                            'text': bibot_server_response['error-codes'][error_code],
+                            'tag': 'error'
+                        });
+                    }
+                } else {
+                    messages.push({
+                        'text': 'بی‌بات به درستی حل نشده است!',
+                        'tag': 'error'
+                    });
+                }
+                res.render('example.ejs', {messages: messages});
+            })
+        } else {
+            res.render('example.ejs', {
+                messages: [{
+                    'text': 'بی‌بات به درستی حل نشده است!',
+                    'tag': 'error'
+                }]
+            });
+        }
+    } else {
+        res.render('example.ejs', {
+            messages: [
+                {
+                    'text': 'ارتباط با سرور بی‌بات برقرار نشده است! آیا جاوااسکریپت شما فعال است؟',
+                    'tag': 'error'
+                }
+            ]
+        });
+    }
 });
 
 
